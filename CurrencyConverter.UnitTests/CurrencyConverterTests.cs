941bbe9c3f5d0ffce0f4fd28bf6e4d3e09772c96
Kinley Penjor <kinley.penjor@developertools.com>
2026-04-20 16:06:17 +0600

    test: test default value

---
+++ CurrencyConverter.UnitTests/CurrencyConverterTests.cs
@@ -0,0 +1,19 @@
+﻿namespace CurrencyConverter.UnitTests
+{
+    internal class CurrencyConverterTests
+    {
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void SourceCurrency_WithDefaultConstructor_ReturnsDefaultValue()
+        {
+            // Arrange - create situation
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act - call func/act for esting ,user interfa
+
+            // Assert - write checks
+            Assert.That(curConverter.SourceCurrency, Is.EqualTo("USD"));
+
+        }
+    }
+}
