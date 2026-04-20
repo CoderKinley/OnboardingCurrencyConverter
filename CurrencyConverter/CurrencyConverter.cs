e2ef7dbb213d670fd2543f8adc54df70cf665f3d
Kinley Penjor <kinley.penjor@developertools.com>
2026-04-20 15:46:17 +0600

    add: dependency propert

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CurrencyConverter
+{
+    public class CurrencyConverter : Control
+    {
+        static CurrencyConverter()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(CurrencyConverter),
+                new FrameworkPropertyMetadata(typeof(CurrencyConverter))
+            );
+        }
+
+        #region Property
+        public string SourceCurrency 
+        {
+            get => (string)GetValue(SourceCurrencyProperty);
+            set => SetValue(SourceCurrencyProperty, value);
+        }
+        
+        #endregion
+
+        #region Dependency Property
+        private static readonly DependencyProperty SourceCurrencyProperty =
+            DependencyProperty.Register(
+                name: nameof(SourceCurrency),
+                propertyType: typeof(bool),
+                ownerType: typeof(CurrencyConverter),
+                typeMetadata: new PropertyMetadata("USD", OnSourceCurrencyChanged)
+            );
+        #endregion
+
+        #region Events
+        public event EventHandler? SourceCurrencyChanged;
+        #endregion
+
+        #region Callbacks
+        private static void OnSourceCurrencyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            ((CurrencyConverter)obj).SourceCurrencyChanged?.Invoke(obj, EventArgs.Empty);
+        }
+        #endregion
+    }
+}
