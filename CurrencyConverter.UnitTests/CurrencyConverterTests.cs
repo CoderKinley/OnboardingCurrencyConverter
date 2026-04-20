9e8af0e1adc8d91dfb082bc7931f5653bc2dc4c3
Kinley Penjor <kinley.penjor@developertools.com>
2026-04-20 16:48:10 +0600

    add: dependency properties

--- CurrencyConverter.UnitTests/CurrencyConverterTests.cs
+++ CurrencyConverter.UnitTests/CurrencyConverterTests.cs
@@ -1,19 +1,61 @@
-﻿namespace CurrencyConverter.UnitTests
+﻿using NUnit.Framework;
+using System.Threading;
+
+namespace CurrencyConverter.UnitTests
 {
+    [TestFixture]
     internal class CurrencyConverterTests
     {
         [Test]
         [Apartment(ApartmentState.STA)]
         public void SourceCurrency_WithDefaultConstructor_ReturnsDefaultValue()
         {
-            // Arrange - create situation
+            // Arrange
             CurrencyConverter curConverter = new CurrencyConverter();
 
-            // Act - call func/act for esting ,user interfa
+            // Act 
 
-            // Assert - write checks
+            // Assert
             Assert.That(curConverter.SourceCurrency, Is.EqualTo("USD"));
+        }
 
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void TargetCurrency_WithDefaultConstructor_ReturnsDefaultValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act 
+
+            // Assert
+            Assert.That(curConverter.TargetCurrency, Is.EqualTo("EUR"));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void SourceValue_WithDefaultConstructor_ReturnsDefaultValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act 
+
+            // Assert 
+            Assert.That(curConverter.SourceValue, Is.EqualTo(1.00m));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void TargetValue_WithDefaultConstructor_ReturnsDefaultValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act 
+
+            // Assert 
+            Assert.That(curConverter.TargetValue, Is.EqualTo(1.00m));
         }
     }
 }
--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CurrencyConverter
@@ -19,7 +20,27 @@ namespace CurrencyConverter
             get => (string)GetValue(SourceCurrencyProperty);
             set => SetValue(SourceCurrencyProperty, value);
         }
-        
+
+        public string TargetCurrency
+        {
+            get => (string)(GetValue(TargeCurrencyProperty));
+            set => SetValue(TargeCurrencyProperty, value);
+        }
+
+
+        public decimal SourceValue
+        {
+            get => (decimal)(GetValue(SourceValueProperty));
+            set => SetValue(SourceValueProperty, value);
+        }
+
+        public decimal TargetValue
+        {
+            get => (decimal)(GetValue(TargetValueProperty));
+            set => SetValue(TargetValueProperty, value);
+        }
+
+
         #endregion
 
         #region Dependency Property
@@ -30,16 +51,61 @@ namespace CurrencyConverter
                 ownerType: typeof(CurrencyConverter),
                 typeMetadata: new PropertyMetadata("USD", OnSourceCurrencyChanged)
             );
+
+
+        private static readonly DependencyProperty TargeCurrencyProperty =
+            DependencyProperty.Register(
+                name: nameof(TargetCurrency),
+                propertyType: typeof(string),
+                ownerType: typeof(CurrencyConverter),       
+                typeMetadata: new PropertyMetadata("EUR", OnTargetCurrencyChanged)
+            );
+
+
+        private static readonly DependencyProperty SourceValueProperty =
+            DependencyProperty.Register(
+                name: nameof(SourceValue),
+                propertyType: typeof(decimal),
+                ownerType: typeof(CurrencyConverter),
+                typeMetadata: new PropertyMetadata(1.00m, OnSourceValueChanged)
+            );
+
+        private static readonly DependencyProperty TargetValueProperty =
+            DependencyProperty.Register(
+                name: nameof(TargetValue),
+                propertyType: typeof(decimal),
+                ownerType: typeof(CurrencyConverter),
+                typeMetadata: new PropertyMetadata(1.00m, OnTargetValueChanged)
+            );
+
         #endregion
 
         #region Events
         public event EventHandler? SourceCurrencyChanged;
+        public event EventHandler? TargetCurrencyChanged;
+        public event EventHandler? SourceValueChanged;
+        public event EventHandler? TargetValueChanged;
         #endregion
 
         #region Callbacks
         private static void OnSourceCurrencyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
         {
             ((CurrencyConverter)obj).SourceCurrencyChanged?.Invoke(obj, EventArgs.Empty);
+        }
+
+        private static void OnTargetCurrencyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            ((CurrencyConverter)obj).TargetCurrencyChanged?.Invoke(obj, EventArgs.Empty);
+        }
+
+        private static void OnSourceValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            ((CurrencyConverter)obj).SourceValueChanged?.Invoke(obj, EventArgs.Empty);
+        }
+
+        private static void OnTargetValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            ((CurrencyConverter)obj).TargetValueChanged?.Invoke(obj, EventArgs.Empty);
         }
         #endregion
     }
