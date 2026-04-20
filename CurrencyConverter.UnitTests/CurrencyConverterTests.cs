a191053f92f052202dc1334430579b95e68f4440
Kinley Penjor <kinley.penjor@developertools.com>
2026-04-20 17:37:33 +0600

    test: new test for events and value changes check

--- CurrencyConverter.UnitTests/CurrencyConverterTests.cs
+++ CurrencyConverter.UnitTests/CurrencyConverterTests.cs
@@ -13,8 +13,6 @@ namespace CurrencyConverter.UnitTests
             // Arrange
             CurrencyConverter curConverter = new CurrencyConverter();
 
-            // Act 
-
             // Assert
             Assert.That(curConverter.SourceCurrency, Is.EqualTo("USD"));
         }
@@ -25,8 +23,6 @@ namespace CurrencyConverter.UnitTests
         {
             // Arrange
             CurrencyConverter curConverter = new CurrencyConverter();
-
-            // Act 
 
             // Assert
             Assert.That(curConverter.TargetCurrency, Is.EqualTo("EUR"));
@@ -39,8 +35,6 @@ namespace CurrencyConverter.UnitTests
             // Arrange
             CurrencyConverter curConverter = new CurrencyConverter();
 
-            // Act 
-
             // Assert 
             Assert.That(curConverter.SourceValue, Is.EqualTo(1.00m));
         }
@@ -52,10 +46,136 @@ namespace CurrencyConverter.UnitTests
             // Arrange
             CurrencyConverter curConverter = new CurrencyConverter();
 
-            // Act 
-
             // Assert 
             Assert.That(curConverter.TargetValue, Is.EqualTo(1.00m));
         }
+
+        #region Property Value Tests
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void SourceCurrency_WithNewValue_ReturnsNewValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act
+            curConverter.SourceCurrency = "BTN";
+
+            // Assert
+            Assert.That(curConverter.SourceCurrency, Is.EqualTo("BTN"));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void TargetCurrency_WithNewValue_ReturnsNewValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act
+            curConverter.TargetCurrency = "JPY";
+
+            // Assert
+            Assert.That(curConverter.TargetCurrency, Is.EqualTo("JPY"));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void SourceValue_WithNewValue_ReturnsNewValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act
+            curConverter.SourceValue = 50.0m;
+
+            // Assert
+            Assert.That(curConverter.SourceValue, Is.EqualTo(50.0m));
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void TargetValue_WithNewSourceValue_ReturnsDoubleReflectedValue()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+
+            // Act
+            curConverter.SourceValue = 75.0m;
+
+            // Assert
+            Assert.That(curConverter.TargetValue, Is.EqualTo(75.0m * 2));
+        }
+
+        #endregion
+
+        #region Event Raising Tests
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void SourceCurrency_WithNewValue_RaisesSourceCurrencyChangedEvent()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+            bool eventRaised = false;
+            curConverter.SourceCurrencyChanged += (s, e) => eventRaised = true;
+
+            // Act
+            curConverter.SourceCurrency = "GBP";
+
+            // Assert
+            Assert.That(eventRaised, Is.True);
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void TargetCurrency_WithNewValue_RaisesTargetCurrencyChangedEvent()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+            bool eventRaised = false;
+            curConverter.TargetCurrencyChanged += (s, e) => eventRaised = true;
+
+            // Act
+            curConverter.TargetCurrency = "INR";
+
+            // Assert
+            Assert.That(eventRaised, Is.True);
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void SourceValue_WithNewValue_RaisesSourceValueChangedEvent()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+            bool eventRaised = false;
+            curConverter.SourceValueChanged += (s, e) => eventRaised = true;
+
+            // Act
+            curConverter.SourceValue = 10.0m;
+
+            // Assert
+            Assert.That(eventRaised, Is.True);
+        }
+
+        [Test]
+        [Apartment(ApartmentState.STA)]
+        public void TargetValue_WithNewValue_RaisesTargetValueChangedEvent()
+        {
+            // Arrange
+            CurrencyConverter curConverter = new CurrencyConverter();
+            bool eventRaised = false;
+            curConverter.TargetValueChanged += (s, e) => eventRaised = true;
+
+            // Act
+            curConverter.SourceValue = 20.0m;
+
+            // Assert
+            Assert.That(eventRaised, Is.True);
+        }
+
+        #endregion
     }
 }
