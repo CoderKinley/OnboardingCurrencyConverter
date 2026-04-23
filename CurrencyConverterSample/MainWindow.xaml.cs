e3cb63d7f7f55339bddfebe30fb4a467f5507fd9
Kinley Penjor <kinley.penjor@developertools.com>
2026-04-23 17:47:17 +0600

    add: sample project and implement currency convert control

---
+++ CurrencyConverterSample/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Windows;
+using CurrencyConverter.Models;
+using CurrencyConverter.Provider;
+using CurrencyConverterSample.Mock_Provider;
+
+namespace CurrencyConverterSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string AccessKey = "9c405fd189370fb1f44b28158e27b0d8";
+
+            HttpClient client = new HttpClient();
+            //CurrencyConversionProvider ccp = new CurrencyConversionProvider(AccessKey, client);
+
+            MockCurrencyProvider ccp = new MockCurrencyProvider();
+            MyCurrencyControl.ConversionProvider = ccp;
+
+        }
+
+       
+    }
+}
