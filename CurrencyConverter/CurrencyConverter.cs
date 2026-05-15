a61d72c9e4a397c82db4eb82d9e38c6d4b8da512
Kinley Penjor <kinley.penjor@developertools.com>
2026-05-15 17:28:00 +0600

    format: code format

--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -30,7 +30,7 @@ namespace CurrencyConverter
         /// <summary>
         /// Gets or sets the source currency code.
         /// </summary>
-        public string SourceCurrency 
+        public string SourceCurrency
         {
             get => (string)GetValue(SourceCurrencyProperty);
             set => SetValue(SourceCurrencyProperty, value);
@@ -61,16 +61,16 @@ namespace CurrencyConverter
         public decimal TargetValue
         {
             get => (decimal)(GetValue(TargetValueProperty));
-            private set => SetValue(TargetValueProperty, value);    
+            private set => SetValue(TargetValueProperty, value);
         }
 
         /// <summary>
         /// Gets or sets the provider used for currency conversion.
         /// </summary>
-        public ICurrencyProvider? ConversionProvider 
+        public ICurrencyProvider? ConversionProvider
         {
             get => (ICurrencyProvider?)GetValue(ConversionProviderProperty);
-            set => SetValue(ConversionProviderProperty, value);     
+            set => SetValue(ConversionProviderProperty, value);
         }
 
         /// <summary>
@@ -197,7 +197,7 @@ namespace CurrencyConverter
             DependencyProperty.Register(
                 name: nameof(TargetCurrency),
                 propertyType: typeof(string),
-                ownerType: typeof(CurrencyConverter),       
+                ownerType: typeof(CurrencyConverter),
                 typeMetadata: new PropertyMetadata("EUR", OnTargetCurrencyChanged)
             );
 
@@ -243,34 +243,74 @@ namespace CurrencyConverter
                 );
 
         public static readonly DependencyProperty SourceAmountStyleProperty =
-            DependencyProperty.Register(nameof(SourceAmountStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(SourceAmountStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty TargetAmountStyleProperty =
-            DependencyProperty.Register(nameof(TargetAmountStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(TargetAmountStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty SourceCurrencyStyleProperty =
-            DependencyProperty.Register(nameof(SourceCurrencyStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(SourceCurrencyStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty TargetCurrencyStyleProperty =
-            DependencyProperty.Register(nameof(TargetCurrencyStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(TargetCurrencyStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty SourceAmountBorderStyleProperty =
-            DependencyProperty.Register(nameof(SourceAmountBorderStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(SourceAmountBorderStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty TargetAmountBorderStyleProperty =
-            DependencyProperty.Register(nameof(TargetAmountBorderStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(TargetAmountBorderStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty SourceCurrencyBorderStyleProperty =
-            DependencyProperty.Register(nameof(SourceCurrencyBorderStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(SourceCurrencyBorderStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty TargetCurrencyBorderStyleProperty =
-            DependencyProperty.Register(nameof(TargetCurrencyBorderStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(TargetCurrencyBorderStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty MainBorderStyleProperty =
-            DependencyProperty.Register(nameof(MainBorderStyle), typeof(Style), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(MainBorderStyle), 
+                typeof(Style), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
 
         public static readonly DependencyProperty CurrencyItemTemplateProperty =
-            DependencyProperty.Register(nameof(CurrencyItemTemplate), typeof(DataTemplate), typeof(CurrencyConverter), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                nameof(CurrencyItemTemplate), 
+                typeof(DataTemplate), 
+                typeof(CurrencyConverter), 
+                new PropertyMetadata(null));
         #endregion
 
         #region Events
@@ -332,7 +372,7 @@ namespace CurrencyConverter
             var control = (CurrencyConverter)obj;
             control.ConversionProviderChanged?.Invoke(control, EventArgs.Empty);
 
-            if(args.NewValue is ICurrencyProvider cp)
+            if (args.NewValue is ICurrencyProvider cp)
             {
                 // chaget currencies
                 control.SupportedCurrencies = cp.SupportedCurrencies;
@@ -345,11 +385,11 @@ namespace CurrencyConverter
         private static void OnTargetValueChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
         {
             ((CurrencyConverter)obj).TargetValueChanged?.Invoke(obj, EventArgs.Empty);
-        } 
+        }
 
         private static async Task UpdateTargetValueAsync(CurrencyConverter control, ICurrencyProvider? currencyProvider = null)
         {
-            if(control.ConversionProvider != null)
+            if (control.ConversionProvider != null)
             {
                 try
                 {
@@ -368,7 +408,7 @@ namespace CurrencyConverter
         /// <param name="fromCode">The source currency ISO code.</param>
         /// <param name="toCode">The target currency ISO code.</param>
         /// <returns>The converted value as a decimal.</returns>
-        public async Task<decimal> ConvertCurrency(decimal amount, string fromCode, string toCode, ICurrencyProvider? currencyProvider = null )
+        public async Task<decimal> ConvertCurrency(decimal amount, string fromCode, string toCode, ICurrencyProvider? currencyProvider = null)
         {
             var provider = currencyProvider ?? ConversionProvider;
             if (provider == null) return 0;
